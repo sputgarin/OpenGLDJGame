fd05f654e9b0086e86e187c3da99a14161fd68aa
sputgarin <sputgarin@protonmail.com>
2024-01-25 23:53:45 +0100

    Got triangle working

--- OpenGLDJGame/OpenGLDJGame.cpp
+++ OpenGLDJGame/OpenGLDJGame.cpp
@@ -10,6 +10,11 @@ void error_callback(int error, const char* msg) {
     cerr << " [" << error << "] " << msg << endl;
 }
 
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
 int main() {
 
     glfwSetErrorCallback(error_callback);
@@ -37,15 +42,6 @@ int main() {
     }
     glfwMakeContextCurrent(window);
 
-    // ----- Create Array Buffer on the GPU and copy our vertices to GPU -------
-    float vertices[] = 
-    {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
-    };
-
-
     // Initialize GLAD (connects OpenGL Functions)
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -58,6 +54,75 @@ int main() {
     // Initialization ends here
     // ==================================================================
     // The Real Program starts here
+    float red = 0;
+
+    // ----- Create Vertex Array Object, which makes changing between VBOs easier -----
+    unsigned int VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // ----- Create Array Buffer on the GPU and copy our vertices to GPU -------
+    float vertices[] = 
+    {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    // Variable to store buffer id
+    unsigned int VBO;
+
+    // ask open gl to create a buffer
+    glGenBuffers(1, &VBO); 
+
+    // tell opengl to use this buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // copy our vertices to the buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // ----- Compile the Vertex Shader on the GPU -------
+    const char* vertexShaderSource = "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "void main()\n"
+        "{\n"
+        " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\0";
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    // ------ Compile the Fragment Shader on the GPU --------
+    const char* fragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "} \0";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // -------- Create Shader Program (Render Pipeline) ---------
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // clean up shaders after they've been linked into a program
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // ------ configure vertex attribute(s) (currently it's just one, the position) -----
+// so the vertex shader understands, where to find the input attributes, in this case position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
 
     // While the User doesn't want to Quit (X Button, Alt+F4)
     while (!glfwWindowShouldClose(window))
@@ -66,13 +131,21 @@ int main() {
         glfwPollEvents();
         processInput(window);
 
+        red += 0.001f;
+        if (red > 1)
+            red -= 1;
+
         // render (paint the current frame of the game)
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // present (send the current frame to the computer screen)
         glfwSwapBuffers(window); // ??
     }
+    glfwTerminate();
+    return 0;
 }
 
 void processInput(GLFWwindow* window)
