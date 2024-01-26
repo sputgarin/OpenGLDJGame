a510d08334eecf81699c2ad6c60d7407544feab7
sputgarin <sputgarin@protonmail.com>
2024-01-26 22:59:09 +0100

    Exercises done

--- OpenGLExerciseCode/OpenGLExerciseCode.cpp
+++ OpenGLExerciseCode/OpenGLExerciseCode.cpp
@@ -61,19 +61,15 @@ int main() {
     float red = 0;
 
     // ----- Create Vertex Array Object, which makes changing between VBOs easier -----
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    glBindVertexArray(VAO);
+    unsigned int playerMesh;
+    glGenVertexArrays(1, &playerMesh);
+    glBindVertexArray(playerMesh);
 
     // ----- Create Array Buffer on the GPU and copy our vertices to GPU -------
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f,
-
-         1.0f,  1.0f, 0.0f,
-         2.0f,  2.0f, 0.0f,
-         0.0f,  1.0f, 0.0f,
+        -1.0f, -0.5f, 0.0f,
+         0.0f, -0.5f, 0.0f,
+         -0.5f,  0.5f, 0.0f,
     };
 
     unsigned int VBO; // variable to store buffer id
@@ -81,6 +77,35 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, VBO); // tell gl to use this buffer
     glBufferData(GL_ARRAY_BUFFER, // copy our vertices to the buffer
         sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // ------ configure vertex attribute(s) (currently it's just one, the position) -----
+    // so the vertex shader understands, where to find the input attributes, in this case position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+
+    // ----- Create Vertex Array Object, which makes changing between VBOs easier -----
+    unsigned int enemyMesh;
+    glGenVertexArrays(1, &enemyMesh);
+    glBindVertexArray(enemyMesh);
+
+    // ----- Create Array Buffer on the GPU and copy our vertices to GPU -------
+    float vertices2[] = {
+        -0.0f, -0.5f, 0.0f,
+         1.0f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+    };
+
+    unsigned int VBO2; // variable to store buffer id
+    glGenBuffers(1, &VBO2); // ask open gl to create a buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO2); // tell gl to use this buffer
+    glBufferData(GL_ARRAY_BUFFER, // copy our vertices to the buffer
+        sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+
+    // ------ configure vertex attribute(s) (currently it's just one, the position) -----
+    // so the vertex shader understands, where to find the input attributes, in this case position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     // ----- Compile the Vertex Shader on the GPU -------
 
@@ -95,33 +120,53 @@ int main() {
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
 
-    // ------ Compile the Fragment Shader on the GPU --------
-    const char* fragmentShaderSource = "#version 330 core\n"
+    // ------ Compile the Orange Fragment Shader on the GPU --------
+    const char* orangeFragmentShaderSource = "#version 330 core\n"
         "out vec4 FragColor;\n"
         "void main()\n"
         "{\n"
         "    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
         "} \0";
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
+    unsigned int orangeFragmentShader;
+    orangeFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(orangeFragmentShader, 1, &orangeFragmentShaderSource, NULL);
+    glCompileShader(orangeFragmentShader);
 
-    // -------- Create Shader Program (Render Pipeline) ---------
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glUseProgram(shaderProgram);
+    // ------ Compile the Yellow Fragment Shader on the GPU --------
+    const char* yellowFragmentShaderSource = "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+        "} \0";
+    unsigned int yellowFragmentShader;
+    yellowFragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(yellowFragmentShader, 1, &yellowFragmentShaderSource, NULL);
+    glCompileShader(yellowFragmentShader);
+
+
+    // -------- Create Orange Shader Program (Render Pipeline) ---------
+    unsigned int orangeShaderProgram;
+    orangeShaderProgram = glCreateProgram();
+    glAttachShader(orangeShaderProgram, vertexShader);
+    glAttachShader(orangeShaderProgram, orangeFragmentShader);
+    glLinkProgram(orangeShaderProgram);
+    
+
+
+    // -------- Create Yellow Shader Program (Render Pipeline) ---------
+    unsigned int yellowShaderProgram;
+    yellowShaderProgram = glCreateProgram();
+    glAttachShader(yellowShaderProgram, vertexShader);
+    glAttachShader(yellowShaderProgram, yellowFragmentShader);
+    glLinkProgram(yellowShaderProgram);
+    
     // clean up shaders after they've been linked into a program
     glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glDeleteShader(orangeFragmentShader);
+    glDeleteShader(yellowFragmentShader);
 
-    // ------ configure vertex attribute(s) (currently it's just one, the position) -----
-    // so the vertex shader understands, where to find the input attributes, in this case position
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
+
 
     // While the User doesn't want to Quit (X Button, Alt+F4)
     while (!glfwWindowShouldClose(window))
@@ -129,15 +174,21 @@ int main() {
         // process input (e.g. close window on Esc)
         glfwPollEvents();
         processInput(window);
-        red += 0.001f;
-        if (red > 1)
-            red -= 1;
+        // red += 0.001f;
+        // if (red > 1)
+        //    red -= 1;
 
         // render (paint the current frame of the game)
         glClearColor(red, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glUseProgram(orangeShaderProgram);
+        glBindVertexArray(playerMesh);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glUseProgram(yellowShaderProgram);
+        glBindVertexArray(enemyMesh);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // present (send the current frame to the computer screen)
         glfwSwapBuffers(window); // ??
